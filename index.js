e7002fb10eee13942be91606d00c0d209b5ba670
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2022-12-27 14:06:23 +0100

    Revert "Update main script and package type"
    This reverts commit b35b3601ddd2fa906ac7d224325348751a62e8f0.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import "@actions/core";
-import "@actions/github";
-import * as fs from "fs";
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
 
 try {
     const git_email = core.getInput('git-email');
     const git_username = core.getInput('git-username');
     let target_version = core.getInput('target-version');
     const target_file = core.getInput('target-file');
-    const target_branch = core.getInput('target-branch');
 
     // generate three random numbers from 0-9
     const random1 = Math.floor(Math.random() * 10);
@@ -21,23 +20,19 @@ try {
     console.log(`Target version without prefix: ${target_version}`);
     console.log(`Username: ${git_username}. Email: ${git_email}`);
 
+    // open the metadata file and load its contents into a string
+
     let metadata = fs.readFileSync(target_file, 'utf8');
     console.log(`Metadata file contents: ${metadata}`);
 
+    // replace the match for the \"version\":(\s*)\"(.*)\" expression with \"version\": \"{target_version}\"
     metadata = metadata.replace(/\"version\":(\s*)\"(.*)\"/, `"version": "${target_version}"`);
     console.log(`Metadata file contents after replacement: ${metadata}`);
-
+    // write the new contents to the metadata file
     fs.writeFileSync(target_file, metadata, 'utf8');
 
+    // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-
-    //configure git with the username and email
-    await exec.exec('git', ['config', '--global', 'user.email', git_email]);
-    await exec.exec('git', ['config', '--global', 'user.name', git_username]);
-
-    await exec.exec('git', ['commit', target_file, '-m', `Update version to ${target_version}`]);
-    await exec.exec('git', ['push', 'origin', `HEAD:${target_branch}`]);
-
 
 } catch (error) {
     core.setFailed(error.message);
