b9e2500df3a5e043b60321f0bbfe82f1e6fc4f9a
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2022-12-27 13:50:44 +0100

    Update compiled index

--- dist/index.js
+++ dist/index.js
@@ -9686,19 +9686,39 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(5127);
 const github = __nccwpck_require__(3134);
+const fs = __nccwpck_require__(7147);
 
 try {
-
     const git_email = core.getInput('git-email');
     const git_username = core.getInput('git-username');
+    const target_version = core.getInput('target-version');
+    const target_file = core.getInput('target-file');
+
+    // generate three random numbers from 0-9
+    const random1 = Math.floor(Math.random() * 10);
+    const random2 = Math.floor(Math.random() * 10);
+    const random3 = Math.floor(Math.random() * 10);
+
+    console.log(`Target version: ${target_version}`);
+    target_version = `refs/tags/v${random1}.${random2}.${random3}`;
+    console.log(`Setting version to a default '${target_version}' for debugging purposes`);
+    target_version = target_version.replace(/^refs\/tags\/v/, '');
+    console.log(`Target version without prefix: ${target_version}`);
     console.log(`Username: ${git_username}. Email: ${git_email}`);
+
+    // open the metadata file and load its contents into a string
+
+    const metadata = fs.readFileSync(target_file, 'utf8');
+    console.log(`Metadata file contents: ${metadata}`);
+
+    // replace the match for the \"version\":(\s*)\"(.*)\" expression with \"version\": \"{target_version}\"
+    metadata = metadata.replace(/\"version\":(\s*)\"(.*)\"/, `"version": "${target_version}"`);
+    console.log(`Metadata file contents after replacement: ${metadata}`);
+    // write the new contents to the metadata file
+    fs.writeFileSync(target_file, metadata, 'utf8');
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
 
 } catch (error) {
     core.setFailed(error.message);
